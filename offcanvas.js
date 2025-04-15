40ec16edde6d3edf9bf29dbe7c206c7feb5075ff
00miK <michaeltarei77@gmail.com>
2025-04-15 12:18:58 +0200

    feat: bascule visibilité menu off-canvas avec JS

---
+++ offcanvas.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar-menu");
+    const toggleButton = document.getElementById("sidebarToggle");
+
+    if (toggleButton && sidebar) {
+      toggleButton.addEventListener("click", function () {
+        sidebar.classList.toggle("active");
+      });
+    }
+});
+
+
+  
